769bbae9e67feefa6c7f60d46b3b627b59e95de3
takwasassi <saif.ajengui@esprit.tn>
2023-04-25 11:05:14 +0100

    commit

--- webapp/controller/Modification.controller.js
+++ webapp/controller/Modification.controller.js
@@ -1,21 +1,47 @@
 sap.ui.define([
 	'./BaseController',
-	'sap/ui/model/json/JSONModel',
-	'sap/m/p13n/Engine',
-	'sap/m/p13n/SelectionController',
-	'sap/m/p13n/SortController',
-	'sap/m/p13n/GroupController',
-	'sap/m/p13n/MetadataHelper',
-	'sap/ui/model/Sorter'
+	"sap/ui/model/odata/v2/ODataModel",
+	"sap/ui/core/util/MockServer",
+	"sap/m/MessageToast",
+	"sap/ui/model/resource/ResourceModel",
+	"sap/ui/core/syncStyleClass",
+	"sap/ui/core/Fragment",
+	"sap/ui/model/json/JSONModel"
 	
-], function (BaseController, JSONModel, Engine, SelectionController, SortController, GroupController, MetadataHelper, Sorter) {
+], function (BaseController,ODataModel, MockServer, MessageToast, ResourceModel, syncStyleClass, Fragment,JSONModel) {
 	"use strict";
 	return BaseController.extend("aymax.pfe.inventaire.controller.Modification", {
 		
 
 		onInit: function() {
-		
+			var oModel = this.getView().getModel();
+			var sSet = "/" + "ImmobilisationSet";
+			oModel.read(sSet, {
+				success: function (oData) {
+					var oModelMNA = new JSONModel();
+					oModelMNA.setData(oData.results);
+					this.getView().setModel(oModelMNA, "oModelMNA");
+				}.bind(this),
+				error: function (oResponse) {
+					sap.m.MessageToast.show("oData fetching failed");
+				}
+			});
 		},
+
+		onInitialise: function (oEvent) {
+
+			var oTable = oEvent.getSource().getTable();
+			var aColumns = oTable.getColumns();
+
+			for (var i = 0; i < aColumns.length; i++) {
+				var sPath = "oModelMNA>" + aColumns[i].data("p13nData").columnKey;
+				aColumns[i].getTemplate().getDisplay().bindText(sPath);
+				aColumns[i].getTemplate().getEdit().bindValue(sPath);
+			}
+
+			oTable.bindRows("oModelMNA>/");
+		},
+		
 		
 	});
 });
